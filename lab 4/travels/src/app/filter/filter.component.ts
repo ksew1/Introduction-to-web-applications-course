2adc854163e26965b998b87ab0e8fed5ee1b837d
ksew1 <95349104+ksew1@users.noreply.github.com>
2023-01-26 20:54:42 +0100

    Add files via upload

---
+++ lab 4/travels/src/app/filter/filter.component.ts
@@ -0,0 +1,33 @@
+import { UpperCasePipe } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl, FormGroup} from '@angular/forms';
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.css']
+})
+export class FilterComponent {
+  @Output() filterEvent = new EventEmitter();
+  @Input() minPrice: any
+  @Input() maxPrice: any
+  filterForm = new FormGroup({
+    name: new FormControl(''),
+    country: new FormControl(''),
+    priceMin: new FormControl(''),
+    priceMax: new FormControl(''),
+    startDate: new FormControl(''),
+    endDate: new FormControl(''),
+    ratingMin: new FormControl(''),
+    ratingMax: new FormControl('')
+  });
+
+
+
+
+  onSubmit() {
+    const formValue = this.filterForm.value
+    this.filterEvent.emit(formValue)
+  }
+
+}
